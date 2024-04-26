311542b3fe9eb288e7afdaa55b9df54f9369cf88
Franco <franconahuelr>
2024-04-26 15:22:52 -0300

    update

---
+++ src/components/Projects/index.js
@@ -0,0 +1,36 @@
+import React from 'react'
+import { useState } from 'react'
+import { Container, Wrapper, Title, Desc, CardContainer, ToggleButtonGroup, ToggleButton, Divider } from './ProjectsStyle'
+import ProjectCard from '../Cards/ProjectCards'
+import { projects } from '../../data/constants'
+
+
+const Projects = ({openModal,setOpenModal}) => {
+  const [toggle, setToggle] = useState('all');
+  return (
+    <Container id="projects">
+      <Wrapper>
+        <Title>Proyectos</Title>
+        <Desc>
+          Algunos de los proyectos en los que estuve trabajando en estos años!. <br />
+          Nota: En mi GitHub hay muchos proyectos de consola.
+          <br />
+          <br />
+        </Desc>
+        <CardContainer>
+          {toggle === 'all' && projects
+            .map((project) => (
+              <ProjectCard project={project} openModal={openModal} setOpenModal={setOpenModal}/>
+            ))}
+          {projects
+            .filter((item) => item.category === toggle)
+            .map((project) => (
+              <ProjectCard project={project} openModal={openModal} setOpenModal={setOpenModal}/>
+            ))}
+        </CardContainer>
+      </Wrapper>
+    </Container>
+  )
+}
+
+export default Projects
---
+++ src/data/constants.js
@@ -0,0 +1,219 @@
+export const Bio = {
+  name: "Franco Romero",
+  roles: [
+    "Desarrollador Frontend",
+  ],
+  description:
+    "Soy un desarrollador junior con una sólida base de conocimientos adquirida durante dos años de formación. Aunque soy nuevo en la industria, estoy ansioso por aplicar mis habilidades y aprender de cada experiencia. Mi meta está en crecer profesionalmente mientras contribuyo con entusiasmo y dedicación a proyectos desafiantes. Estoy emocionado por la oportunidad de trabajar en equipo y contribuir al éxito de cualquier proyecto que se me presente.",
+  github: "https://github.com/franconahuelr",
+  resume:
+    "https://drive.google.com/file/d/1IHOLhZPi2jysY6VCZ6QXhYsrssP7f-lE/view?usp=sharing",
+  linkedin: "https://www.linkedin.com/in/franco-romerods/",
+};
+
+export const skills = [
+  {
+    title: "Frontend",
+    skills: [
+      {
+        name: "React Js",
+        image:
+          "data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHZpZXdCb3g9Ii0xMS41IC0xMC4yMzE3NCAyMyAyMC40NjM0OCI+CiAgPHRpdGxlPlJlYWN0IExvZ288L3RpdGxlPgogIDxjaXJjbGUgY3g9IjAiIGN5PSIwIiByPSIyLjA1IiBmaWxsPSIjNjFkYWZiIi8+CiAgPGcgc3Ryb2tlPSIjNjFkYWZiIiBzdHJva2Utd2lkdGg9IjEiIGZpbGw9Im5vbmUiPgogICAgPGVsbGlwc2Ugcng9IjExIiByeT0iNC4yIi8+CiAgICA8ZWxsaXBzZSByeD0iMTEiIHJ5PSI0LjIiIHRyYW5zZm9ybT0icm90YXRlKDYwKSIvPgogICAgPGVsbGlwc2Ugcng9IjExIiByeT0iNC4yIiB0cmFuc2Zvcm09InJvdGF0ZSgxMjApIi8+CiAgPC9nPgo8L3N2Zz4K",
+      },
+      {
+        name: "HTML",
+        image: "https://www.w3.org/html/logo/badge/html5-badge-h-solo.png",
+      },
+      {
+        name: "CSS",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d5/CSS3_logo_and_wordmark.svg/1452px-CSS3_logo_and_wordmark.svg.png",
+      },
+      {
+        name: "JavaScript",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/6/6a/JavaScript-logo.png/800px-JavaScript-logo.png",
+      },
+      {
+        name: "Bootstrap",
+        image:
+          "https://getbootstrap.com/docs/5.3/assets/brand/bootstrap-logo-shadow.png",
+      }
+    ],
+  },
+  {
+    title: "Backend",
+    skills: [
+      {
+        name: "Python",
+        image:
+          "https://raw.githubusercontent.com/devicons/devicon/master/icons/python/python-original.svg",
+      },
+      {
+        name: "MySQL",
+        image:
+          "https://raw.githubusercontent.com/devicons/devicon/master/icons/mysql/mysql-original-wordmark.svg",
+      },
+      {
+        name: "Firebase",
+        image: "https://www.vectorlogo.zone/logos/firebase/firebase-icon.svg",
+      },
+    ],
+  },
+  {
+    title: "Otras",
+    skills: [
+      {
+        name: "Git",
+        image:
+          "https://git-scm.com/images/logos/downloads/Git-Icon-1788C.png",
+      },
+      {
+        name: "GitHub",
+        image:
+          "https://github.githubassets.com/images/modules/logos_page/GitHub-Mark.png",
+      },
+      {
+        name: "Docker",
+        image:
+          "https://raw.githubusercontent.com/devicons/devicon/master/icons/docker/docker-original-wordmark.svg",
+      },
+      {
+        name: "Netlify",
+        image:
+          "https://seeklogo.com/images/N/netlify-logo-BD8F8A77E2-seeklogo.com.png",
+      },
+      {
+        name: "VS Code",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/9/9a/Visual_Studio_Code_1.35_icon.svg/512px-Visual_Studio_Code_1.35_icon.svg.png?20210804221519",
+      },
+      {
+        name: "Postman",
+        image:
+          "https://cdn.worldvectorlogo.com/logos/postman.svg",
+      }
+    ],
+  },
+];
+
+export const experiences = [
+  {
+    //futura experiencia
+  }
+];
+
+export const education = [
+  {
+    id: 0,
+    img: "https://www.editores-srl.com.ar/sites/default/files/logos/utn_regional_rosario_200x200px.jpg",
+    school: "Universidad Tecnologica Nacional",
+    date: "Abril 2021 - Actualidad 2024",
+    grade: "8 CGPA",
+    desc: "Actualmente estoy en el ultimo cuatrimestre de la carrera donde he aprendido diversas tecnologias tanto para back-end como para front-end de las cuales he hecho proyectos tanto individuales como en grupo, mejorando asi mis habilidades como trabajo en equipo y liderazgo",
+    degree: "Tecnico Universitario en Programacion",
+  }
+
+];
+
+export const projects = [
+  {
+    id: 0,
+    title: "E-Commerce",
+    date: "Septiembre 2023 - Noviembre 2023",
+    description:
+      "Desarrolle un E-commerce de venta de ropa, el cual cuenta con una base de datos, sistema de inicio de sesion y carrito",
+    image:
+      "https://iili.io/JSU5zcF.md.png",
+    tags: ["React Js", "Firebase", "CSS", "Boostrap"],
+    category: "web app",
+    github: "https://github.com/franconahuelr/ecommerce-ropa-lc3",
+    webapp: "https://ecommerce-ropa-lc3.vercel.app/",
+    member: [
+      {
+        name: "Franco Romero",
+        img: "https://media.licdn.com/dms/image/D4E03AQGHkq7MQ9kC6w/profile-displayphoto-shrink_800_800/0/1713755920688?e=1719446400&v=beta&t=wLf6_LRYcHEV_9XlZY9FeOoOQNP1X8h_s0xJVKK1g-Q",
+        linkedin: "https://www.linkedin.com/in/franco-romerods/",
+        github: "https://github.com/franconahuelr",
+      }
+    ],
+    
+  },
+  {
+  id: 1,
+  title: "App Web Clima",
+  date: "Abril 2022 - Mayo 2022",
+  description:
+    "Desarrolle junto con una compañera una web conectada a una API con la cual podes consultar el clima cargando la ciudad en LocalStorage",
+  image:
+    "https://iili.io/JSt6Jbs.md.png",
+  tags: ["Javascript","CSS", "HTML5"],
+  category: "web app",
+  github: "https://github.com/franconahuelr/lab2_tpi",
+  webapp: "/",
+  member: [
+    {
+      name: "Franco Romero",
+      img: "https://media.licdn.com/dms/image/D4E03AQGHkq7MQ9kC6w/profile-displayphoto-shrink_800_800/0/1713755920688?e=1719446400&v=beta&t=wLf6_LRYcHEV_9XlZY9FeOoOQNP1X8h_s0xJVKK1g-Q",
+      linkedin: "https://www.linkedin.com/in/franco-romerods/",
+      github: "https://github.com/franconahuelr",
+    },
+    {
+      name: "Virginia Vaccaro"
+    }
+  ],
+  },
+  {
+    id: 2,
+    title: "Cotizador Web",
+    date: "Julio 2021 - Agosto 2021",
+    description:
+      "Esta fue uno de mis primeros proyectos aplicando logica con Javascript",
+    image:
+      "https://iili.io/JStpHxI.md.png",
+    tags: ["Javascript", "CSS", "HTML5"],
+    category: "web app",
+    github: "https://github.com/franconahuelr/lab2_cotizador",
+    webapp: "/",
+    member: [
+      {
+        name: "Franco Romero",
+        img: "https://media.licdn.com/dms/image/D4E03AQGHkq7MQ9kC6w/profile-displayphoto-shrink_800_800/0/1713755920688?e=1719446400&v=beta&t=wLf6_LRYcHEV_9XlZY9FeOoOQNP1X8h_s0xJVKK1g-Q",
+        linkedin: "https://www.linkedin.com/in/franco-romerods/",
+        github: "https://github.com/franconahuelr",
+      }
+    ],
+    
+  },
+  {
+    id: 3,
+    title: "Calculadora de Costos",
+    date: "Julio 2021 - Agosto 2021",
+    description:
+      "Esta fue uno de mis primeros proyectos aplicando logica con Javascript",
+    image:
+      "https://iili.io/JU7mk6F.md.png",
+    tags: ["Javascript", "CSS", "HTML5"],
+    category: "web app",
+    github: "https://github.com/franconahuelr/calculadora-epe",
+    webapp: "/",
+    member: [
+      {
+        name: "Franco Romero",
+        img: "https://media.licdn.com/dms/image/D4E03AQGHkq7MQ9kC6w/profile-displayphoto-shrink_800_800/0/1713755920688?e=1719446400&v=beta&t=wLf6_LRYcHEV_9XlZY9FeOoOQNP1X8h_s0xJVKK1g-Q",
+        linkedin: "https://www.linkedin.com/in/franco-romerods/",
+        github: "https://github.com/franconahuelr",
+      }
+    ],
+    
+  },
+];
+
+/*export const TimeLineData = [
+  { year: 2017, text: "Started my journey" },
+  { year: 2018, text: "Worked as a freelance developer" },
+  { year: 2019, text: "Founded JavaScript Mastery" },
+  { year: 2020, text: "Shared my projects with the world" },
+  { year: 2021, text: "Started my own platform" },
+];
+*/
